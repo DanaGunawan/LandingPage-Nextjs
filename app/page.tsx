bb90230c17ee5459228cd7a9444fa2b62903c80d
Dana Gunawan <dewakadekdana196@gmail.com>
2025-05-27 11:51:01 +0800

    Landing Page Deploy

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import Script from "next/script"
+import CallToActionWithAnnotation from "@/components/HeroSection"
+import SimpleThreeColumns from "@/components/Features"
+import SplitWithImage from "@/components/Testimonials"
+
+export default function Home() {
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-J3W8SDFWRN"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-J3W8SDFWRN');
+        `}
+      </Script>
+
+      <CallToActionWithAnnotation />
+      <SimpleThreeColumns />
+      <SplitWithImage />
+    </>
+  )
+}
